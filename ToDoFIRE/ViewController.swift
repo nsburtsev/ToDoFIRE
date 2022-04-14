6e426cdcfdd7b342f1abbadfc3868ec32c53bda4
Nyurgun <burtsev.ns@gmail.com>
2022-04-14 22:19:46 +0900

    Main View interface

---
+++ ToDoFIRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by Нюргун on 14.04.2022.
+//  Copyright © 2022 Нюргун. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
