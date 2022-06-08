7a0c0372bfb0c4f079b0edf51a181757b7715d86
nsburtsev <burtsev.ns@gmail.com>
2022-06-09 00:27:20 +0900

    Firebase pods, keyboard issue

--- ToDoFIRE/Controller/LoginViewController.swift
+++ ToDoFIRE/Controller/LoginViewController.swift
@@ -16,18 +16,31 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(kbAppear), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDisappear), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
     }
-
+    
+    @objc func kbAppear(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + kbFrameSize.height)
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+    }
+    
+    @objc func kbDisappear() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+    }
+    
     @IBAction func loginTapped(_ sender: UIButton) {
     }
     
     @IBAction func registerTapped(_ sender: UIButton) {
     }
+    
 }
 
